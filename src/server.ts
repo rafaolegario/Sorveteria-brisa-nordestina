9e7ce6514dcf2c2c2c28fe472c0e010449de9358
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-02-17 16:31:55 -0300

    add import ejs

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as ejs from 'ejs'
 import { router } from './routes/routes'
 import { protectedRouter } from './routes/protected';
 import { authRouter } from './routes/auth';
@@ -8,21 +9,22 @@ import cookieParser from "cookie-parser";
 const cors = require('cors');
 
 
-
 dotenv.config();
 const app = express()
-app.engine("ejs", require("ejs").__express);
+
 app.use(cors());
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "../public")))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,"..",'views'))
+app.engine("ejs", ejs.renderFile);
 app.use(express.json());
 app.use("/Brisanordestina",router)
 app.use(express.urlencoded({extended:true}))
 app.use("/Brisanordestina",router)
 app.use("/auth", authRouter)
 app.use("/protected", protectedRouter)
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
